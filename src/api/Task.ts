65405a1753d73655f67d5c198b0f20267e959576
Алексей Ломачев <aleksey@Noutbuk-Aleksej.local>
2025-05-14 22:28:58 +0300

    Begin of semireport

--- src/api/Task.ts
+++ src/api/Task.ts
@@ -1,8 +1,17 @@
 import axios from 'axios';
 
+type Prompt = {
+  prompt_name: string;
+  prompt: string;
+}
+
 export const createTranscriptionTask = async (conversationId: string): Promise<void> => {
   await axios.post(`/api/task/create/convert/${conversationId}`);
 };
+
+export const createSemiReportTask = async (conversationId: string, prompt: Prompt): Promise<void> => {
+  await axios.post(`/api/task/create/semireport/${conversationId}`, prompt);
+}
 
 // export const createDiarizeTask = async (conversationId: string): Promise<void> => {
 //   await axios.post(`/api/task/create/diarize/${conversationId}`);
